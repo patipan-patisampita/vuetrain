2bbb97960dc1971d22298f0d4c9aa9ff20a009ca
patipan-patisampita <patipan.patisampita@gmail.com>
2021-07-03 00:51:18 +0700

    update vue

---
+++ src/main.js
@@ -0,0 +1,13 @@
+import { createApp } from "vue";
+import App from "./App.vue";
+import router from "./router";
+import store from "./store";
+
+//import "bootstrap/dist/css/bootstrap.css"; //bootstrap 5 css
+import "bootstrap/dist/js/bootstrap.bundle"; //bootstrap 5 js (bootstrap+Popper.js)
+
+//admin template
+import "startbootstrap-sb-admin/dist/css/styles.css"; //รวม bootstrap 5 มาแล้ว
+import "startbootstrap-sb-admin/dist/js/scripts.js";
+
+createApp(App).use(store).use(router).mount("#app");
---
+++ src/views/About.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="about">
+    <h1>เกี่ยวกับเรา</h1>
+    <Logo />
+  </div>
+</template>
+
+<script>
+import Logo from '../components/Logo.vue'
+
+export default {
+  name: "About",
+  components: {
+    Logo,
+  }
+};
+</script>
+
+<style scoped>
+  .about{
+    color: red;
+  }
+</style>
